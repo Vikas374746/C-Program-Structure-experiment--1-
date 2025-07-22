7b1dabe9196c9d51d5df2b1f36fe2a1da7de2a5b
Vikas Jayavarapu <vikasjayavarapu12@gmail.com>
2025-07-22 15:38:12 +0530

    Update and rename exp-1 hello world.ccp to exp-1 hello world.cpp

---
+++ exp-1 hello world.cpp
@@ -0,0 +1,17 @@
+// NAME: Vikas J
+// PRN: 24070123166
+// Exp-1
+
+// Program-1
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    cout << "First Program";
+    return 0;
+}
+/*
+Hello World
+First Program
+*/
